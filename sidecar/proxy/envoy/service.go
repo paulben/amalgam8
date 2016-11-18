d138c89d3b1b6dbf68bb1f4ab0f0017e4bd88ec7
Isaiah Snell-Feikema <ijsnellf@us.ibm.com>
2016-11-18 17:34:45 +0000

    Fixed Envoy hot restart logic.

--- sidecar/proxy/envoy/service.go
+++ sidecar/proxy/envoy/service.go
@@ -3,6 +3,7 @@ package envoy
 import (
 	"fmt"
 	"os/exec"
+	"syscall"
 )
 
 type Service interface {
@@ -24,24 +25,32 @@ func NewService(config string) Service {
 }
 
 func (s *service) Start() error {
-	if s.cmd == nil {
+	if s.cmd != nil {
 		return nil
 	}
 
 	s.cmd = exec.Command("envoy", "-c", s.config, "--restart-epoch", fmt.Sprint(s.restartEpoch))
-	return s.cmd.Start() // Wait() to prevent zombies?!
+	return s.cmd.Start()
 }
 
+// Reload
 func (s *service) Reload() error {
 	if s.cmd != nil {
 		s.cmd.Process.Release()
 	}
 
-	s.restartEpoch++
 	s.cmd = exec.Command("envoy", "-c", s.config, "--restart-epoch", fmt.Sprint(s.restartEpoch))
-	return s.cmd.Start() // Wait() to prevent zombies?!
+	s.restartEpoch++
+
+	return s.cmd.Start()
 }
 
+// Stop Envoy
+// Envoy propagates the SIGTERM to all running instances of Envoy (I.E., Envoy instances with lower
+// restart-epoch numbers.)
 func (s *service) Stop() error {
-	return s.cmd.Process.Kill()
+	if err := s.cmd.Process.Signal(syscall.SIGTERM); err != nil {
+		return err
+	}
+	return s.cmd.Process.Release()
 }
